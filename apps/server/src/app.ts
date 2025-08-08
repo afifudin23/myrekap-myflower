c2e64215faa976ff91150ae6e320c8d1cfb8637e
afifudin23 <afifu5882@gmail.com>
2025-08-08 21:13:15 +0700

    deploy testing 3

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,46 +1,46 @@
+// import cors from "cors";
+// import rootRouter from "./routes";
+// import { attachLogger, errorMiddleware, httpLogger } from "./middlewares";
+// import helmet from "helmet";
+// import { InternalException } from "./exceptions";
+// import ErrorCode from "./constants/error-code";
+// import cookieParser from "cookie-parser";
+// import path from "path";
+// const allowedOrigins = env.CLIENT_ORIGINS.split(",") || [];
+// import { env } from "@/config";
 import express from "express";
-import cors from "cors";
-import rootRouter from "./routes";
-import { attachLogger, errorMiddleware, httpLogger } from "./middlewares";
-import helmet from "helmet";
-import { InternalException } from "./exceptions";
-import ErrorCode from "./constants/error-code";
-import cookieParser from "cookie-parser";
-import path from "path";
-import { env } from "@/config";
 const app = express();
-const allowedOrigins = env.CLIENT_ORIGINS.split(",") || [];
 // middlewares
-app.use(helmet()); // for security
-app.use(
-    cors({
-        origin: (origin, callback) => {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
-        credentials: true, // Allow cookies to be sent
-        exposedHeaders: ["Content-Disposition"], // Expose Content-Disposition header for file downloads
-    })
-); // for cross-origin requests
-app.use(express.static(path.join(__dirname, "..", "public")));
-app.use(express.json()); // for parsing json
-app.use(attachLogger); // for logging requests with request id
-app.use(httpLogger); // for logging http requests
-app.use(cookieParser());
+// app.use(helmet()); // for security
+// app.use(
+//     cors({
+//         origin: (origin, callback) => {
+//             if (!origin || allowedOrigins.includes(origin)) {
+//                 callback(null, true);
+//             } else {
+//                 callback(new Error("Not allowed by CORS"));
+//             }
+//         },
+//         credentials: true, // Allow cookies to be sent
+//         exposedHeaders: ["Content-Disposition"], // Expose Content-Disposition header for file downloads
+//     })
+// ); // for cross-origin requests
+// app.use(express.static(path.join(__dirname, "..", "public")));
+// app.use(express.json()); // for parsing json
+// app.use(attachLogger); // for logging requests with request id
+// app.use(httpLogger); // for logging http requests
+// app.use(cookieParser());
 
-app.use("/hello", (_req, res) => {
+app.get("/hello", (_req, res) => {
     res.send("Hello World");
 });
 
-app.get("/test-error", async (_req, _res, next) => {
-    next(new InternalException("Something went wrong", ErrorCode.INTERNAL_EXCEPTION, null));
-});
+// app.get("/test-error", async (_req, _res, next) => {
+//     next(new InternalException("Something went wrong", ErrorCode.INTERNAL_EXCEPTION, null));
+// });
 
-app.use("/api", rootRouter);
+// app.use("/api", rootRouter);
 
-app.use(errorMiddleware); // error handling middleware
+// app.use(errorMiddleware); // error handling middleware
 
 export default app;
