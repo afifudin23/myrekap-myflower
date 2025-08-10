ff0588d57a1af4242af73ba1df29f9e3c9562c1b
afifudin23 <afifu5882@gmail.com>
2025-08-10 22:46:09 +0700

    deploy testing 19

--- apps/server/api/index.ts
+++ apps/server/api/index.ts
@@ -1,4 +1,11 @@
+import { prisma } from "@/config";
 import app from "../src/app";
 import "module-alias/register";
+import seedSuperadmin from "@/seeds/seed-superadmin";
+
+(async () => {
+    await prisma.$connect();
+    await seedSuperadmin();
+})();
 
 export default app;
