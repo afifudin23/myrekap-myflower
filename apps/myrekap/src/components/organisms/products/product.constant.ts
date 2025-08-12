a630f8494790c9ed1ae5ba4461b691c63ffe3050
afifudin23 <afifu5882@gmail.com>
2025-08-12 08:26:49 +0700

    deploy testing 23

--- apps/myrekap/src/components/organisms/products/product.constant.ts
+++ apps/myrekap/src/components/organisms/products/product.constant.ts
@@ -34,7 +34,7 @@ export const PRODUCT_STOCK_FORM_FIELDS = [
         label: "Tipe Perubahan Stok",
         type: "dropdown",
         name: "type",
-        options: TYPE_STOCK_REPORT_ITEMS.filter((item) => item.label !== "General"),
+        options: TYPE_STOCK_REPORT_ITEMS.filter((item) => item !== "General"),
         optionLabel: TYPE_STOCK_REPORT_LABELS,
     },
     {
