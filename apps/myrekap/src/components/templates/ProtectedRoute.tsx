948932a17035c3a4facd3628337e9a9733f7d986
afifudin23 <afifu5882@gmail.com>
2025-08-15 18:28:23 +0700

    deploy testing 26

--- apps/myrekap/src/components/templates/ProtectedRoute.tsx
+++ apps/myrekap/src/components/templates/ProtectedRoute.tsx
@@ -1,23 +1,23 @@
 // src/components/routes/ProtectedRoute.tsx
-import { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { useEffect } from "react";
 import { useAuthStore } from "@/stores/useAuthStore";
 import { Loading } from "@/components/atoms";
 
 const ProtectedRoute = () => {
-    const { isAuthenticated, loading, verifyToken } = useAuthStore();
+    // const { isAuthenticated, loading, verifyToken } = useAuthStore();
 
-    useEffect(() => {
-        verifyToken();
-    }, []);
+    // useEffect(() => {
+    //     verifyToken();
+    // }, []);
 
-    if (loading) return <Loading />;
+    // if (loading) return <Loading />;
 
-    return isAuthenticated ? (
+    // return isAuthenticated ? (
         <Outlet />
-    ) : (
-        <Navigate to="/auth/login" state={{ message: "Sesi Anda Telah Habis, Silahkan Login Kembali" }} replace />
-    );
+    // ) : (
+    // <Navigate to="/auth/login" state={{ message: "Sesi Anda Telah Habis, Silahkan Login Kembali" }} replace />;
+    // );
 };
 
 export default ProtectedRoute;
